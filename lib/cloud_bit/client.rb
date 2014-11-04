80634d3efef68139eca1ecc6ba863e56e30ebc6d
Brian Kelly <polymonic@gmail.com>
2014-11-04 07:02:30 -0500

    Adds a few constants to the Client class.

--- lib/cloud_bit/client.rb
+++ lib/cloud_bit/client.rb
@@ -2,18 +2,21 @@ require 'rest_client'
 
 module CloudBit
   class Client
+    API_URL = 'https://api-http.littlebitscloud.cc'
+    API_VERSION = 'v2'
+
     def initialize(access_token, device_id)
       @access_token = access_token
       @device_id = device_id
     end
 
     def output(percent, duration)
-      RestClient.post "https://api-http.littlebitscloud.cc/devices/#{device_id}/output",
+      RestClient.post "#{API_URL}/devices/#{device_id}/output",
         {
           :percent => percent.to_s,
           :duration_ms => duration.to_s
         },
-        Accept: "application/vnd.littlebits.v2+json",
+        Accept: "application/vnd.littlebits.#{API_VERSION}+json",
         Authorization: "Bearer #{access_token}"
     end
 
